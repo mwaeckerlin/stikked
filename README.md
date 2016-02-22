cd3506f777c3d113773fc2870f37a7456b85ba53
Marc Wäckerlin <marc@waeckerlin.org>
2016-02-22 23:20:19 +0100

    works

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# stikked
-Docker Image for Stikker — an open source paste-bin replacement
+# Docker Image for Stikker — an open source paste-bin replacement
+
+This image provides the volume only.
+
+Connect mwaeckerlin/stikked with a mysql database, a
+mwaeckerlin/php-fpm and a mwaeckerlin/nginx to get the full
+functionality:
+
+    docker run -d --name -mysql \
+               -e MYSQL_DATABASE=stikkerdb \
+               -e MYSQL_USER=stikker \
+               -e MYSQL_PASSWORD=$(pwgen 20 1) \
+               -e MYSQL_RANDOM_ROOT_PASSWORD=1 \
+               mysql
+    
+    docker run -d --name stikked-volume \
+               --link sticked-mysql:mysql \
+               mwaeckerlin/stikked
+    
+    docker run -d --name stikked-php \
+               --link sticked-mysql:mysql \
+               --volumes-from stikked-volume  \
+               mwaeckerlin/php-fpm
+    
+    docker run -d -p 8080:80 --name stikked-nginx \
+               --link stikked-php:php \
+               --volumes-from stikked-volume \
+               mwaeckerlin/nginx
+
---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+export MYSQL_LINK_NAME=$(env | sed -n 's,_NAME=/.*,,p')
+dbvar="${MYSQL_LINK_NAME}_ENV_MYSQL_DATABASE"
+uservar="${MYSQL_LINK_NAME}_ENV_MYSQL_USER"
+pwdvar="${MYSQL_LINK_NAME}_ENV_MYSQL_PASSWORD"
+if test -z "${MYSQL_LINK_NAME}"; then
+    echo "**** ERROR: no link to mysql database found" 1>&2
+    exit 1
+fi
+if test -z "${!dbvar}"; then
+    echo "**** ERROR: initialize mysql in ${MYSQL_LINK_NAME,,*} with -e MYSQL_DATABASE" 1>&2
+    exit 1
+fi
+if test -z "${!uservar}"; then
+    echo "**** ERROR: initialize mysql in ${MYSQL_LINK_NAME,,*} with -e MYSQL_USER" 1>&2
+    exit 1
+fi
+if test -z "${!pwdvar}"; then
+    echo "**** ERROR: initialize mysql in ${MYSQL_LINK_NAME,,*} with -e MYSQL_PASSWORD" 1>&2
+    exit 1
+fi
+sed -e 's,\(\$config\[.db_hostname.\] = \).*;,\1'"'${MYSQL_LINK_NAME,,*}'"';,' \
+    -e 's,\(\$config\[.db_database.\] = \).*;,\1'"'${!dbvar}'"';,' \
+    -e 's,\(\$config\[.db_username.\] = \).*;,\1'"'${!uservar}'"';,' \
+    -e 's,\(\$config\[.db_password.\] = \).*;,\1'"'${!pwdvar}'"';,' \
+    /stikked/application/config/stikked.php.dist \
+    > /stikked/application/config/stikked.php
+sleep infinity
